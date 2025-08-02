48206ad78b3756c3c750456b47e5863374725a76
Hikari <58058800+hikario@users.noreply.github.com>
2025-08-01 21:30:16 -0700

    adjusted some ui in bath game segment

--- Assets/Scripts/WebVideo.cs
+++ Assets/Scripts/WebVideo.cs
@@ -7,13 +7,7 @@ public class WebVideo : MonoBehaviour
 {
     [SerializeField] string videoFileName;
 
-    //public bool destroyAfterPlay;
-    //public float frameCount;
-    //public GameObject videoGO;
-
     private VideoPlayer videoPlayer;
-
-    //private bool isPrepared;
 
     void Awake()
     {
@@ -29,35 +23,6 @@ public class WebVideo : MonoBehaviour
         if (videoPlayer)
         {
             videoPlayer.Play();
-            //CalculateVideoLength();
-            //Debug.Log("framecount is " + frameCount);
-            //if (destroyAfterPlay)
-            //{
-            //    StartCoroutine(Frames());
-            //}
         }
     }
-
-    //void CalculateVideoLength()
-    //{
-    //    // Get frame count and frame rate from the VideoPlayer. 
-    //    ulong fC = videoPlayer.frameCount;
-    //    float frameRate = videoPlayer.frameRate; ;
-
-    //    frameCount = fC / frameRate;
-    //}
-
-    //IEnumerator Frames()
-    //{
-    //    if (frameCount >= 0)
-    //    {
-    //        yield return new WaitForSeconds(frameCount);
-    //        DestroyVideoGO();
-    //    }
-    //}
-
-    //void DestroyVideoGO()
-    //{
-    //    Destroy(videoGO);
-    //}
 }
